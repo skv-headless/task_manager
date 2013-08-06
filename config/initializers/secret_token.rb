912a249d629e7b996338d2a2f99a729a79d2706a
skv <skv-headless@yandex.ru>
2013-08-06 21:28:15 +0400

    auth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskManager::Application.config.secret_token = '9dc7fbfae538eeec63a26a2707aee01a511a8b58165cd867109ab2119d2c935450cf4a94187202ec14ef231fc6c5c763e30594de672c52baa2609e7b126f7923'
