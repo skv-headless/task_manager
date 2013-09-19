519337513713656df3f622298095eb4129cf4f8b
skv <skv-headless@yandex.ru>
2013-09-19 15:41:47 +0400

    auth methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  private
+  #TODO FIXME сделать Concerns
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def sign_out
+    session[:user_id] = nil
+  end
 
   def authenticate_user!
     redirect_to new_session_path unless session[:user_id]
@@ -10,5 +21,6 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
+
+  helper_method :current_user, :signed_in?
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user.try(:authenticate, params[:password])
-      session[:user_id] = user.id
+      sign_in(user)
       redirect_to root_url, :notice => 'Logged in!'
     else
       flash.now.alert = 'Invalid email or password'
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    sign_out
     redirect_to new_session_path, :notice => 'Logged out!'
   end
 end
