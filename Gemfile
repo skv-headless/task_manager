824f164094e51af7c73d9b7260ed5f86dba36678
skv <skv-headless@yandex.ru>
2013-09-19 09:54:32 +0400

    broken mailer

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ group :development do
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'rack-mini-profiler'
+  gem "letter_opener"
 end
 
 gem 'jquery-rails'
--- app/models/story.rb
+++ app/models/story.rb
@@ -7,10 +7,34 @@ class Story < ActiveRecord::Base
   validates_presence_of :title, :description
 
   state_machine :initial => :new do
-    state :new
-    state :started
-    state :finished
-    state :accepted
-    state :rejected
+    event :new do
+      transition all => :new
+    end
+
+    event :started do
+      transition all => :started
+    end
+
+    event :finished do
+      transition all => :finished
+    end
+
+    event :accepted do
+      transition all => :accepted
+    end
+
+    event :rejected do
+      transition all => :rejected
+    end
+  end
+
+  after_save :send_assignment_email, :if => :send_assignment_email?
+
+  def send_assignment_email
+    StoryMailer.assignment_email(self).deliver
+  end
+
+  def send_assignment_email?
+    'assigned_to_id'.in?(self.changed) && self.assigned_to_id.kind_of?(Integer)
   end
 end
