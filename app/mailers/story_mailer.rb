300f3bfcf6050bdf2d196674c1506e5377e3f0a3
skv <skv-headless@yandex.ru>
2013-09-20 11:56:48 +0400

    mail subject throw i18n api

--- app/mailers/story_mailer.rb
+++ app/mailers/story_mailer.rb
@@ -2,10 +2,8 @@ class StoryMailer < ActionMailer::Base
   default from: "skv-headless@yandex.ru"
 
   def assignment_email(story)
-    #сделано просто для примера
-    #TODO добавить подписчиков к Story
     @user = story.assigned_to
     @story = story
-    mail(to: @user.email, subject: "Stroy notification")
+    mail(to: @user.email)
   end
 end
