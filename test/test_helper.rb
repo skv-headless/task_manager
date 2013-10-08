57682cc7f57ba92b514e445af8c58fc5d096dc5a
skv <skv-headless@yandex.ru>
2013-10-08 17:04:52 +0400

    Fix проблемы с счетчиком assert-ов

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,12 @@
-require 'coveralls'
-Coveralls.wear!('rails')
-
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+
+#TODO FIXME разобратся почему не работает адаптер(проблема с иерархией)
+#require 'wrong/adapters/minitest'
+
+require 'coveralls'
+Coveralls.wear!('rails')
 
 FactoryGirl.reload
 
@@ -15,6 +18,17 @@ class ActiveSupport::TestCase
   fixtures :all
 
   include Wrong
+
+  def failure_class
+    MiniTest::Assertion
+  end
+
+  def aver(valence, explanation = nil, depth = 0)
+    self._assertions += 1 # increment minitest's assert count
+    super(valence, explanation, depth + 1) # apparently this passes along the default block
+  end
+
+
   include FactoryGirl::Syntax::Methods
   include Concerns::AuthManagement
 end
